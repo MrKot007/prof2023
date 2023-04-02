abdd188e1b2d75177574502973899be7d3cebb3c
MrKot007 <arseniy.bobovnikov@gmail.com>
2023-04-02 22:05:08 +0300

    test2

--- app/src/main/java/com/example/codemania/OnBoardingActivity.kt
+++ app/src/main/java/com/example/codemania/OnBoardingActivity.kt
@@ -7,5 +7,6 @@ class OnBoardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_boarding)
+
     }
 }
---
+++ app/src/main/java/com/example/codemania/OnBoardingFragment.kt
@@ -0,0 +1,24 @@
+package com.example.codemania
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.codemania.databinding.FragmentBoardingBinding
+
+class OnBoardingFragment(val text: String, val img: Int) : Fragment() {
+    private lateinit var bindingFragment: FragmentBoardingBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return bindingFragment.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        bindingFragment.boardingText.text = this.text
+        bindingFragment.img.setImageResource(this.img)
+    }
+}
