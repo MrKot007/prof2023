82e16edcc0f42e222ee547e5522b0e61829a2bfa
MrKot007 <arseniy.bobovnikov@gmail.com>
2023-04-02 21:52:20 +0300

    test

--- app/src/main/java/com/example/codemania/SplashActivity.kt
+++ app/src/main/java/com/example/codemania/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.example.codemania
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.codemania.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -9,6 +12,8 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashActivity, OnBoardingActivity::class.java))
+        }, 1000)
     }
 }
